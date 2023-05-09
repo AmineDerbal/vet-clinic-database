6cf9a8a3d082ed3819202226cfeafd1438435850
Amine derbal <aminedoran31@gmail.com>
2023-05-09 14:40:05 +0100

    Create indexes for join table treatment_history

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -79,4 +79,7 @@ CREATE INDEX patient_id_index ON medical_histories(patient_id);
 CREATE INDEX medical_history_id_index ON invoices(medical_history_id);
 CREATE INDEX invoice_id_index ON invoice_items(invoice_id);
 CREATE INDEX treatment_id_index ON invoice_items(treatment_id);
+CREATE INDEX treatment_id_joinIndex ON treatment_history(treatment_id);
+CREATE INDEX medical_history_id_joinIndex ON treatment_history(medical_history_id);
+
 
