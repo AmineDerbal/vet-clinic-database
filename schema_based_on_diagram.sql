b28f4750d202a25092f81021ac6fccc5b824bd40
Amine derbal <aminedoran31@gmail.com>
2023-05-09 14:37:13 +0100

    create treatment_id_index for invoice_items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -78,3 +78,5 @@ CREATE TABLE treatment_history(
 CREATE INDEX patient_id_index ON medical_histories(patient_id);
 CREATE INDEX medical_history_id_index ON invoices(medical_history_id);
 CREATE INDEX invoice_id_index ON invoice_items(invoice_id);
+CREATE INDEX treatment_id_index ON invoice_items(treatment_id);
+
