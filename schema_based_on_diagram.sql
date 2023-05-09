1b52525b9f16e30c5bfb2c53c1b96a412b87a1b6
Amine derbal <aminedoran31@gmail.com>
2023-05-09 13:49:27 +0100

    create table invoices

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -19,3 +19,16 @@ CREATE TABLE medical_histories(
   FOREIGN KEY(patient_id)
   REFERENCES patients(id) 
 );
+
+-- create table invoices 
+CREATE TABLE invoices(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  total_amount decimal,
+  generated_at timestamp,
+  payed_at timestamp,
+  medical_history_id int,
+  primary key(id),
+  CONSTRAINT fk_medical_history
+  FOREIGN KEY(medical_history_id)
+  REFERENCES medical_histories(id)
+);
